18cc99a80ca1c9a1996b66d8f1cd4a46734f9c7d
Maryonete <mauricemarion@hotmail.com>
2024-02-19 12:21:52 +0100

    installe phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,10 +2,30 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
+if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
+    require dirname(__DIR__) . '/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
+
+// clean environment each time
+(new \Symfony\Component\Filesystem\Filesystem())->remove(__DIR__ . '/../var/cache/test');
+
+// Clean database
+$env = 'test';
+// passthru(sprintf(
+//     'php bin/console cache:clear',
+//     $env
+// ));
+
+// Définir une variable d’environnement        putenv('DATABASE_URL=mysql://testuser:testpass@localhost/testdb');
+// dump($parameter->get('event.listener.enabled'));
+
+// Initialiser la base de données de test
+shell_exec('php bin/console d:d:d --force --env=test');
+shell_exec('php bin/console d:d:c --env=test');
+shell_exec('php bin/console doctrine:database:create --env=test');
+shell_exec('php bin/console doctrine:schema:update --complete --force --env=test');
+shell_exec('php bin/console doctrine:fixtures:load -n --env=test');
