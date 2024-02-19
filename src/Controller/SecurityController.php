e017a78a0706df0776fb619a3023c748c65c8cae
Maryonete <mauricemarion@hotmail.com>
2024-02-19 18:44:21 +0100

    add lexik/jwt-authentication-bundle

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
 
 class SecurityController extends AbstractController
 {
@@ -28,7 +36,29 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function apiLogin(
+        Request $request,
+        JWTTokenManagerInterface $jwtManager,
+        UserRepository $usersRepository
+    ): JsonResponse {
+        // Récupérer les données d'identification envoyées par l'application ElectronJS
+        $data = json_decode($request->getContent(), true);
+        $email = $data['username'] ?? null;
+        $password = $data['password'] ?? null;
 
+        // Trouver l'utilisateur correspondant à l'e-mail
+        $user = $usersRepository->findOneBy(['email' => $email]);
+        //        $passwordHasher = $this->container->get('security.password_hasher');
+        // Vérifier les identifiants de l'utilisateur
+        if (!$user || !password_verify($password, $user->getPassword())) {
+            throw new AuthenticationException('Identifiants invalides');
+        }
+
+        $token = $jwtManager->create($user);
+
+        return new JsonResponse(['token' => $token]);
+    }
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
