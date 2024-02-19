151f19bbbed98981e2c1ec8a45fe1078237e693f
Maryonete <mauricemarion@hotmail.com>
2024-02-19 08:48:23 +0100

    creation entity User

---
+++ src/Entity/User.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Exception;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+use TypeError;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: 'user')]
+#[ApiResource]
+#[UniqueEntity(fields: ['email'], message: 'Il y a déjà un compte avec cet email')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    #[Assert\Length(min: 2, max: 180)]
+    #[Assert\Email(
+        message: 'Cet email {{ value }} n\'est pas valide',
+    )]
+    private ?string $email = null;
+
+    /**
+     * @var array The list of roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 60)]
+    #[Assert\Length(min: 2, max: 60)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 60)]
+    #[Assert\Length(min: 2, max: 60)]
+    private ?string $lastname = null;
+
+    public function __toString()
+    {
+        return $this->getFirstName() . '  ' . $this->getLastName();
+    }
+
+    public function toArray(): array
+    {
+        return [
+            "id"    => $this->getId(),
+            "name"  => $this->getLastName() . "  " . $this->getFirstName(),
+        ];
+    }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new TypeError('Format de l\'Email Incorrect');
+        } else {
+            $this->email = $email;
+        }
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // // guarantee every user at least has ROLE_USER
+        // $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+    public function isAdmin(): bool
+    {
+        return in_array("ROLE_ADMIN", $this->getRoles());
+    }
+    public function isUser(): bool
+    {
+        return in_array("ROLE_USER", $this->getRoles());
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+}
