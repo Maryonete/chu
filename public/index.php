68ef2c501a05aa28c1e960e8b766e13d75103734
Maryonete <mauricemarion@hotmail.com>
2024-03-06 10:14:55 +0100

    test heroku : erreur Authenticator does not support the request

--- public/index.php
+++ public/index.php
@@ -6,5 +6,5 @@ require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     var_dump('ca marche ' . $context['APP_ENV'] . ' ---- ' . $context['APP_DEBUG']);
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    //return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
